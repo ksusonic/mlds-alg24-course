e3d115912a47477a3b676fdee9afb28ab40210d7
Daniil <ksusonic@yandex.ru>
2024-05-14 19:50:32 +0300

    Solve D

---
+++ contests/3_binary_search_sorts/D.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    nums = list(map(int, input().split()))
+
+    slow = nums[0]
+    fast = nums[0]
+
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+
+    print(slow)
+
