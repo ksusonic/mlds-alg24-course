e82322429f53de36f6edf2f3cda873ccea583788
Daniil <ksusonic@yandex.ru>
2024-06-01 15:01:05 +0300

    Solve A

---
+++ contests/5_sort_recursion_dynamics/B.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.setrecursionlimit(3000)
+
+
+def fast_power(a, n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return a
+
+    if n // 2 == 0:
+        return fast_power(a**2, n // 2)
+    return a * fast_power(a, n - 1)
+
+
+if __name__ == "__main__":
+    print(fast_power(float(input()), int(input())))
