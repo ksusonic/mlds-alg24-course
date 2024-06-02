7e90df1b4d20bc26925281d97edee4c8ae51accc
Daniil <ksusonic@yandex.ru>
2024-06-02 16:36:15 +0300

    Solve B

--- contests/5_sort_recursion_dynamics/B.py
+++ contests/5_sort_recursion_dynamics/B.py
@@ -1,18 +1,25 @@
-import sys
+def solution(line: str) -> int:
+    res = line.count("B")
+    line = line.replace("B", "")
 
-sys.setrecursionlimit(3000)
+    cur_side = "L"
+    for i in range(len(line)):
+        if line[i] != cur_side:
+            continue
 
+        if (i + 1 < len(line) and line[i] == line[i + 1]) or (
+            i + 1 == len(line) and line[i] == "L"
+        ):
+            cur_side = "R" if cur_side == "L" else "L"
 
-def fast_power(a, n):
-    if n == 0:
-        return 1
-    if n == 1:
-        return a
+        res += 1
 
-    if n // 2 == 0:
-        return fast_power(a**2, n // 2)
-    return a * fast_power(a, n - 1)
+    if cur_side != "R":
+        res += 1
+
+    return res
 
 
 if __name__ == "__main__":
-    print(fast_power(float(input()), int(input())))
+    line = input()
+    print(solution(line))
